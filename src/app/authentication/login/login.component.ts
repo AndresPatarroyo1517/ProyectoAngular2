cdbaf6bb5c67bbee2a89ec25227277f5e20feae7
Andres Patarroyo <A@gmail.com>
2024-05-20 16:01:34 -0500

    Ejemplo

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -23,6 +23,7 @@ export class LoginComponent {
     this.authenticationService.login(form.value).subscribe(
       (res) => {
         localStorage.setItem('accessToken',JSON.parse(JSON.stringify(res)).accessToken);
+        localStorage.setItem('Caballero', '¿Como esta usted?')
         this.router.navigateByUrl('/animal');
       }
     );
