1989ea57f8700e7f09643b0fd98780a0b97f85dd
Andres Patarroyo <A@gmail.com>
2024-05-20 15:41:15 -0500

    Arreglos propios

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -9,7 +9,7 @@ import { Jwtres } from '../models/jwtres';
   providedIn: 'root'
 })
 export class AuthenticationService {
-  apiUri = '/api';
+  apiUri = '/api2';
   authSubject = new BehaviorSubject(false);
   private token: string | null = '';
 
